365a10c277258ab13ff36099753f2767d36d2b6d
vova <varuss729@gmail.com>
2023-02-04 16:56:19 +0200

    increase revision version every time the script run

--- changeversion.ps1
+++ changeversion.ps1
@@ -1,11 +1,8 @@
-
-
-
 $newVersion=$args[0]
 function Set-CSProj-Version{
 Param(
 [parameter(Mandatory=$false)] [String] $filePath="$(get-location)\ASPBasicserver.csproj",
-[parameter(Mandatory=$true)] [String] $newVersion
+[parameter(Mandatory=$false)] [String] $newVersion
 )
     $csprojfile=New-Object XML
     $csprojfile.Load($filePath)
@@ -14,8 +11,16 @@ Param(
         Write-Host "Version is null."
         $child = $csprojfile.CreateElement("Version")
         $csprojfile.Project.PropertyGroup.AppendChild($child) | out-null
-    }
+        write-host $csprojfile.Project.PropertyGroup.Version
         $csprojfile.Project.PropertyGroup.Version = $newVersion
         $csprojfile.Save($filePath)
+    }
+    else {
+        $v=[version]$csprojfile.Project.PropertyGroup.Version
+        $newVersion = "$($v.Major).$($v.Minor).$($v.Build).$($v.Revision + 1)"
+        $csprojfile.Project.PropertyGroup.Version = $newVersion
+        $csprojfile.Save($filePath)
+        }
+
 }
 Set-CSProj-Version -newVersion $newVersion
